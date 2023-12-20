6795f333b16526ef300fc411295e342e65d2fce5
Vanals <marco91vanali@gmail.com>
2023-12-20 14:54:27 +0000

    awaiting write

--- src/pages/api/register-attendance.js
+++ src/pages/api/register-attendance.js
@@ -71,7 +71,7 @@ export default async function handler(req, res) {
         return res.status(400).json({ error: 'Missing required fields' });
       }
 
-      addAttendanceRecord(student_id, class_id, new Date().toISOString())
+      await addAttendanceRecord(student_id, class_id, new Date().toISOString())
 
       return res.status(200).json({ message: 'Attendance marked successfully' });
 
